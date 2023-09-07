061100c2e099a72300aadcf930c6d7df85d03254
Clément Busschaert <clement.busschaert@gmail.com>
2023-09-07 05:34:04 +0200

    chore(nvim): consistency indents (tabs to spaces)

--- stow/nvim/.config/nvim/lua/clembu/packer.lua
+++ stow/nvim/.config/nvim/lua/clembu/packer.lua
@@ -1,14 +1,14 @@
 vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-	-- NAVIGATION
-	use {
-		'nvim-telescope/telescope.nvim', branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use 'theprimeagen/harpoon'
-	use 'tpope/vim-unimpaired'
+    -- NAVIGATION
+    use {
+        'nvim-telescope/telescope.nvim', branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use 'theprimeagen/harpoon'
+    use 'tpope/vim-unimpaired'
     use {
         'echasnovski/mini.files',
         requires = {
@@ -16,37 +16,38 @@ return require('packer').startup(function(use)
         }
     }
 
-	-- HISTORY
-	use 'mbbill/undotree'
-	use 'tpope/vim-fugitive'
+    -- HISTORY
+    use 'mbbill/undotree'
+    use 'tpope/vim-fugitive'
 
-	-- CODE
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
-	use 'nvim-treesitter/playground'
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{
-				'williamboman/mason.nvim',
-				run = function()
-					pcall(vim.api.nvim_command, 'MasonUpdate')
-				end,
-			},
-			{'williamboman/mason-lspconfig.nvim'},
+    -- CODE
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+    use 'nvim-treesitter/playground'
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.api.nvim_command, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'},
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},     -- Required
-			{'hrsh7th/cmp-nvim-lsp'}, -- Required
-			{'L3MON4D3/LuaSnip'},     -- Required
-		}
-	}
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
 
+    -- DECORATIONS
     use 'folke/todo-comments.nvim'
     use {
         'folke/zen-mode.nvim',
@@ -56,14 +57,14 @@ return require('packer').startup(function(use)
     }
     use 'echasnovski/mini.trailspace'
 
-	-- MOTIONS, TEXTOBJECTS
-	use 'tpope/vim-surround'
+    -- MOTIONS, TEXTOBJECTS
+    use 'tpope/vim-surround'
     use 'tpope/vim-commentary'
     use 'echasnovski/mini.operators'
 
-	-- COLORS
-	use {
-		'catppuccin/nvim',
-		as = 'catppuccin'
-	}
+    -- COLORS
+    use {
+        'catppuccin/nvim',
+        as = 'catppuccin'
+    }
 end)
