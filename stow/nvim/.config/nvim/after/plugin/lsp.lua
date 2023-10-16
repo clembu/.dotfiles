d3d64e7d6a5bdb9c1f8bfdd5b1f46c885b03fb00
Clément Busschaert <clement.busschaert@gmail.com>
2023-10-16 11:41:25 +0200

    feat(nvim): Lua LSP diagnostics
    disable lowercase global

--- stow/nvim/.config/nvim/after/plugin/lsp.lua
+++ stow/nvim/.config/nvim/after/plugin/lsp.lua
@@ -7,39 +7,55 @@ require("mason-lspconfig").setup {
 }
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<Tab>'] = cmp.mapping.confirm({select = false}),
-	['<C-Space>'] = cmp.mapping.complete(),
-	['<C-j>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-k>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<Tab>'] = cmp.mapping.confirm({ select = false }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-j>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-k>'] = cmp.mapping.select_next_item(cmp_select),
 })
 lsp.setup_nvim_cmp({ mapping = cmp_mappings })
 
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
-  -- Inspection
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>hd", function() vim.diagnostic.open_float() end, opts)
+    -- Inspection
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>hd", function() vim.diagnostic.open_float() end, opts)
 
-  -- Navigation
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-  vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
-  vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-  vim.keymap.set("n", "(d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", ")d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>rf", function() vim.lsp.buf.format() end, opts)
+    -- Navigation
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "(d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", ")d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>rf", function() vim.lsp.buf.format() end, opts)
 
-  -- Refactoring
-  vim.keymap.set("n", "<leader>ra", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    -- Refactoring
+    vim.keymap.set("n", "<leader>ra", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
 
-  -- Inline help
-  vim.keymap.set("i", "<C-i>", function() vim.lsp.buf.signature_help() end, opts)
+    -- Inline help
+    vim.keymap.set("i", "<C-i>", function() vim.lsp.buf.signature_help() end, opts)
 end)
+
+require("lspconfig").lua_ls.setup({
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT"
+            }
+            ,
+            diagnostics = {
+                disable = {
+                    "lowercase-global"
+                }
+            }
+        }
+    }
+})
 
 lsp.setup()
