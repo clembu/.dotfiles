622029fd8feeb6d6b5a34eece7ac5d2013b0208f
Clément Busschaert <clement.busschaert@gmail.com>
2024-01-04 03:18:06 +0100

    feat(nvim): add treesitter context

--- stow/nvim/.config/nvim/lua/clembu/packer.lua
+++ stow/nvim/.config/nvim/lua/clembu/packer.lua
@@ -26,6 +26,7 @@ return require('packer').startup(function(use)
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
+    use 'nvim-treesitter/nvim-treesitter-context'
     use 'nvim-treesitter/playground'
     use {
         'VonHeikemen/lsp-zero.nvim',
